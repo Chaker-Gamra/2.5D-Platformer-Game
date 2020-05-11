ca9ef2d6514a46e23a29e757da4fbdc08069a5e5
Chaker-Gamra <developer3.5pro@gmail.com>
2020-05-11 09:35:47 +0100

    (E02): Moving The Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 direction;
+    public float speed = 8;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        direction.x = hInput * speed;
+
+        controller.Move(direction * Time.deltaTime);
+    }
+}
