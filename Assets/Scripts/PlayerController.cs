715ee880725be492b9ccd3b3812c592c2a7d5dcd
Chaker-Gamra <developer3.5pro@gmail.com>
2020-05-15 09:10:45 +0100

    (E03): Jump (Double Jump) & Gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     private Vector3 direction;
     public float speed = 8;
 
+    public float jumpForce = 10;
+    public float gravity = -20;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    public bool ableToMakeADoubleJump = true;
     void Start()
     {
         
@@ -18,6 +24,26 @@ public class PlayerController : MonoBehaviour
     {
         float hInput = Input.GetAxis("Horizontal");
         direction.x = hInput * speed;
+
+        bool isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
+        if (isGrounded)
+        {
+            direction.y = -1;
+            ableToMakeADoubleJump = true;
+            if (Input.GetButtonDown("Jump"))
+            {
+                direction.y = jumpForce;
+            }
+        }
+        else
+        {
+            direction.y += gravity * Time.deltaTime;
+            if (ableToMakeADoubleJump && Input.GetButtonDown("Jump"))
+            {
+                direction.y = jumpForce;
+                ableToMakeADoubleJump = false;
+            }
+        }
 
         controller.Move(direction * Time.deltaTime);
     }
