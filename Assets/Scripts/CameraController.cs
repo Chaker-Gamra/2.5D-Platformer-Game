0b759d5862bc61622a8acc90ff7ee2ad026b9d2b
Chaker-Gamra <developer3.5pro@gmail.com>
2020-05-25 18:37:14 +0100

    (E06): Smooth Camera Follow Script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform target;
+    private Vector3 offset;
+
+    public float smoothSpeed = 0.15f;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - target.position;
+    }
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+    }
+}
