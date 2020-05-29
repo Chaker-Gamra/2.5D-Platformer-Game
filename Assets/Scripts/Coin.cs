9789e6d86ced5329bdfecc44070e6c48ee010673
Chaker-Gamra <developer3.5pro@gmail.com>
2020-05-29 08:58:15 +0100

    (E07): Pick Up Coins

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float rotationSpeed = 50;
+
+    void Update()
+    {
+        //Rotate The Coin
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerManager.numberOfCoins++;//Increment the number of coins
+            Destroy(gameObject);//destroy the coin
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static int numberOfCoins;
+    public TextMeshProUGUI numberOfCoinsText;
+
+    void Start()
+    {
+        numberOfCoins = 0;//Reset the number of coins on each level
+    }
+
+    void Update()
+    {
+        //Display the number of coins
+        numberOfCoinsText.text = "coins:" + numberOfCoins;
+    }
+}
