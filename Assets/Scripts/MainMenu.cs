208757f9bd50ac195f88396ea068ff520ad0c959
Chaker-Gamra <developer3.5pro@gmail.com>
2020-07-07 12:00:28 +0100

    (E16): Main Menu & Level Selection

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+}
